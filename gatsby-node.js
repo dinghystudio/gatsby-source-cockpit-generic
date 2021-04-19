5839aa8fad81025ca832beb72350f66f9915e613
Nils Borgböhmer <nils@dinghy.studio>
2021-04-19 12:41:02 +0200

    Fixing API compatibility for cockpit 0.12.0
    - formatting a bunch of code
    - updating packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,21 +1,19 @@
+const { camelCase, merge, upperFirst, toLower, trim } = require("lodash");
+const { createRemoteFileNode } = require("gatsby-source-filesystem");
 
-const { camelCase, merge, upperFirst, toLower, trim } = require('lodash')
-const { createRemoteFileNode } = require('gatsby-source-filesystem')
-
-const Cockpit = require('./api')
-const getContentHelpers = require('./helpers')
-
+const Cockpit = require("./api");
+const getContentHelpers = require("./helpers");
 
 const CONFIG_DEFAULTS = {
-  host: '',
-  accessToken: '',
-  typePrefix: 'CockpitGeneric',
+  host: "",
+  accessToken: "",
+  typePrefix: "CockpitGeneric",
   l10n: {},
   contents: [
-    { type: 'collection', whitelist: [], blacklist: [] },
-    { type: 'singleton', whitelist: [], blacklist: [] },
+    { type: "collection", whitelist: [], blacklist: [] },
+    { type: "singleton", whitelist: [], blacklist: [] },
     {
-      type: 'asset',
+      type: "asset",
       filetypes: {
         image: true,
         video: true,
@@ -30,41 +28,52 @@ const CONFIG_DEFAULTS = {
       },
     },
   ],
-}
-
+};
 
 exports.sourceNodes = async (args, options) => {
   const config = merge(CONFIG_DEFAULTS, options);
 
-  const { l10n, host, accessToken, uploadPath } = config
-  if (!host || !accessToken) throw new Error(
-    'Please specify Cockpit host and accessToken in gatsby-config'
-  )
-  const cockpit = new Cockpit(host, accessToken)
+  const { l10n, host, accessToken, uploadPath } = config;
+  if (!host || !accessToken)
+    throw new Error(
+      "Please specify Cockpit host and accessToken in gatsby-config"
+    );
+  const cockpit = new Cockpit(host, accessToken);
   const contentTypes = [
-    { type: 'collection', multiple: true },
-    { type: 'singleton', multiple: false },
-  ]
+    { type: "collection", multiple: true },
+    { type: "singleton", multiple: false },
+  ];
 
-  const helpers = getContentHelpers(args, config)
-  const { getContentTypeNode, getFieldSpecification } = helpers
+  const helpers = getContentHelpers(args, config);
+  const { getContentTypeNode, getFieldSpecification } = helpers;
 
-  const { actions, cache } = args
-  const { createNode, touchNode } = actions
+  const { actions, cache } = args;
+  const { createNode, touchNode } = actions;
 
-  let links = {}, nodes = [], assetMap = {}
-  assetMap = await fetchAssets(config, assetMap, cockpit, args)
+  let links = {},
+    nodes = [],
+    assetMap = {};
+  assetMap = await fetchAssets(config, assetMap, cockpit, args);
 
   // process content types
-  for (let contentType, TypeNode, types, typeConfig, ct = 0; ct < contentTypes.length; ct++) {
-    ({ type: contentType, multiple } = contentTypes[ct])
+  for (
+    let contentType, TypeNode, types, typeConfig, ct = 0;
+    ct < contentTypes.length;
+    ct++
+  ) {
+    ({ type: contentType, multiple } = contentTypes[ct]);
 
-    typeConfig = config.contents.find(({ type }) => type === contentType)
-    types = await listContentType(cockpit, contentType, typeConfig.whitelist, typeConfig.blacklist)
+    typeConfig = config.contents.find(({ type }) => type === contentType);
+    types = await listContentType(
+      cockpit,
+      contentType,
+      typeConfig.whitelist,
+      typeConfig.blacklist
+    );
 
-    TypeNode = getContentTypeNode(`${contentType}s`)
+    TypeNode = getContentTypeNode(`${contentType}s`);
     for (let type, spec, content, parent, Node, t = 0; t < types.length; t++) {
-      type = types[t]
+      type = types[t];
 
       parent = TypeNode({
         id: upperFirst(`${type}`),
@@ -72,50 +81,54 @@ exports.sourceNodes = async (args, options) => {
         meta: {
           type,
           contentType,
-          category: 'list',
+          category: "list",
         },
-      })
-      nodes = [].concat(nodes, [parent])
+      });
+      nodes = [].concat(nodes, [parent]);
 
-      Node = getContentTypeNode(`${contentType} ${type}`)
+      Node = getContentTypeNode(`${contentType} ${type}`);
 
-      spec = await loadContentTypeSpecification(cockpit, contentType, type)
-      spec = spec.fields.map(
-        (f) => ([f.name, getFieldSpecification(f)])
-      ).reduce(
-        (acc, [name, s]) => ({ ...acc, [name]: s, }),
-        {},
-      )
+      spec = await loadContentTypeSpecification(cockpit, contentType, type);
+      spec = spec.fields
+        .map((f) => [f.name, getFieldSpecification(f)])
+        .reduce((acc, [name, s]) => ({ ...acc, [name]: s }), {});
 
-      content = await loadContentTypeEntries(cockpit, contentType, type)
-      if (!content || !content.entries || !content.entries.length) continue
+      content = await loadContentTypeEntries(cockpit, contentType, type);
+      if (!content || !content.entries || !content.entries.length) continue;
 
       for (let entry, i = 0; i < content.entries.length; i++) {
-        entry = content.entries[i]
+        entry = content.entries[i];
 
         // this is super weird and related to how cockpit api does not supply
         // an _id when fetching actual singleton content
-        if (content._id) entry = { _id: content._id, ...entry }
+        if (content._id) entry = { _id: content._id, ...entry };
         entry = {
           meta: {
             id: entry._id,
             type,
             contentType,
-            category: 'entry',
+            category: "entry",
           },
           ...entry,
-        }
+        };
 
         // wrapper to process an entry into nodes and compile links and language
         // alternatives. Including cache handling to only process changed and new entries
-        const getNodesFromEntry = async (id, entry, l10n, nodes, links, alternates) => {
-          const cacheKey = `${config.typePrefix}_${entry.meta.type}_${id}`
-          const cacheData = await cache.get(cacheKey)
-          const modified = entry._modified
+        const getNodesFromEntry = async (
+          id,
+          entry,
+          l10n,
+          nodes,
+          links,
+          alternates
+        ) => {
+          const cacheKey = `${config.typePrefix}_${entry.meta.type}_${id}`;
+          const cacheData = await cache.get(cacheKey);
+          const modified = entry._modified;
 
           if (cacheData && modified === cacheData.modified) {
-            cachedNodes = cacheData.nodes
-            cachedNodes.map(nodeId => touchNode({ nodeId }))
+            cachedNodes = cacheData.nodes;
+            cachedNodes.map((nodeId) => touchNode({ nodeId }));
           } else {
             processed = processEntry(
               Node,
@@ -124,148 +137,163 @@ exports.sourceNodes = async (args, options) => {
               spec,
               entry,
               assetMap,
-              helpers,
-            )
+              helpers
+            );
 
-            nodes = [].concat(nodes, processed.nodes)
-            parent.children.push(processed.node.id)
+            nodes = [].concat(nodes, processed.nodes);
+            parent.children.push(processed.node.id);
 
             // create a list of ids used for reverse relations later on
-            links = processed.links.reduce((acc, v) => ({
-              ...acc,
-              [v]: [].concat(acc[v] || [], [{ type, id: processed.node.id}]),
-            }), links)
+            links = processed.links.reduce(
+              (acc, v) => ({
+                ...acc,
+                [v]: [].concat(acc[v] || [], [{ type, id: processed.node.id }]),
+              }),
+              links
+            );
 
             // keep track of identical nodes in different languages
-            alternates = [].concat(alternates, [processed.node])
+            alternates = [].concat(alternates, [processed.node]);
 
             await cache.set(cacheKey, {
               modified,
-              nodes: [processed.node.id].concat(processed.nodes.map(n => n.id)),
-            })
+              nodes: [processed.node.id].concat(
+                processed.nodes.map((n) => n.id)
+              ),
+            });
           }
 
-          return { nodes, links, alternates }
-        }
+          return { nodes, links, alternates };
+        };
 
         // create node, process fields for all languages specified in config
-        let alternates = []
+        let alternates = [];
         if (l10n.languages && l10n.languages.length) {
           // process nodes for all languages
           for (let language, nodeId, l = 0; l < l10n.languages.length; l++) {
-            language = l10n.languages[l]
-            nodeId = `${entry.meta.id}-${language}`
+            language = l10n.languages[l];
+            nodeId = `${entry.meta.id}-${language}`;
             const processedNodes = await getNodesFromEntry(
               nodeId,
               entry,
               { language, isDefault: language === l10n.default },
               nodes,
               links,
-              alternates,
-            )
+              alternates
+            );
 
-            nodes = processedNodes.nodes
-            links = processedNodes.links
-            alternates = processedNodes.alternates
+            nodes = processedNodes.nodes;
+            links = processedNodes.links;
+            alternates = processedNodes.alternates;
+            // console.log(nodes)
           }
         } else {
-          nodeId = `${entry.meta.id}`
+          nodeId = `${entry.meta.id}`;
           const processedNodes = await getNodesFromEntry(
             nodeId,
             entry,
             {},
             nodes,
             links,
-            alternates,
-          )
+            alternates
+          );
 
-          nodes = processedNodes.nodes
-          links = processedNodes.links
-          alternates = processedNodes.alternates
+          nodes = processedNodes.nodes;
+          links = processedNodes.links;
+          alternates = processedNodes.alternates;
         }
 
         // link localized nodes to one another
-        alternates = alternates.map(n => ({
+        alternates = alternates.map((n) => ({
           ...n,
-          alternates___NODE: alternates.filter(a => a.id !== n.id).map(a => a.id),
-        }))
+          alternates___NODE: alternates
+            .filter((a) => a.id !== n.id)
+            .map((a) => a.id),
+        }));
 
-        nodes = [].concat(nodes, alternates)
+        nodes = [].concat(nodes, alternates);
       }
     }
   }
 
   // process, create nodes and relation links
   for (let node, nodeLinks, i = 0; i < nodes.length; i++) {
-    node = nodes[i]
+    node = nodes[i];
 
     // process reverse relations
     if (Object.keys(links).includes(node.id)) {
-      nodeLinks = links[node.id]
+      nodeLinks = links[node.id];
       for (let nl, key, l = 0; l < nodeLinks.length; l++) {
-        nl = nodeLinks[l]
-        key = `${nl.type}_set___NODE`
-        if (!node[key]) node[key] = []
-        node[key] = [].concat(node[key], nl.id)
+        nl = nodeLinks[l];
+        key = `${nl.type}_set___NODE`;
+        if (!node[key]) node[key] = [];
+        node[key] = [].concat(node[key], nl.id);
       }
     }
 
     // finally create node
-    createNode(node)
+    createNode(node);
   }
-}
+};
 
+const listContentType = async (
+  cockpit,
+  type,
+  whitelist = [],
+  blacklist = []
+) => {
+  let items = [].concat(whitelist);
+  if (items.length === 0) items = await cockpit[camelCase(`list ${type}s`)]();
 
-const listContentType = async (cockpit, type, whitelist=[], blacklist=[]) => {
-  let items = [].concat(whitelist)
-  if (items.length === 0) items = await cockpit[camelCase(`list ${type}s`)]()
-
-  items = items.filter(i => !blacklist.includes(i))
-  return items
-}
-
+  items = items.filter((i) => !blacklist.includes(i));
+  return items;
+};
 
 const loadContentTypeSpecification = (cockpit, type, item) => {
-  const entries = cockpit[toLower(`${type}`)](item)
-  return entries
-}
-
+  const entries = cockpit[toLower(`${type}`)](item);
+  return entries;
+};
 
 const loadContentTypeEntries = (cockpit, type, item) => {
-  const entries = cockpit[camelCase(`${type}Entries`)](item)
-  return entries
-}
-
+  const entries = cockpit[camelCase(`${type}Entries`)](item);
+  return entries;
+};
 
 const fetchAssets = async (config, assetMap, cockpit, args) => {
-  const { actions, cache, createNodeId, store } = args
-  const { createNode, touchNode } = actions
+  const { actions, cache, createNodeId, store } = args;
+  const { createNode, touchNode } = actions;
 
-  const assetConfig = config.contents.find(({ type }) => type === 'asset')
-  if (!assetConfig) return assetMap
+  const assetConfig = config.contents.find(({ type }) => type === "asset");
+  if (!assetConfig) return assetMap;
 
-  const { uploadPath } = config
-  const { filetypes, tags } = assetConfig
+  const { uploadPath } = config;
+  const { filetypes, tags } = assetConfig;
 
   // fetch assets and create remote file nodes
-  const { entries: assets } = await cockpit.assets()
-  for (let asset, cacheKey, cacheData, modified, url, file, a = 0; a < assets.length; a++) {
-    let nodeId = undefined
-    asset = assets[a]
+  const { entries: assets } = await cockpit.assets();
+  for (
+    let asset, cacheKey, cacheData, modified, url, file, a = 0;
+    a < assets.length;
+    a++
+  ) {
+    let nodeId = undefined;
+    asset = assets[a];
 
-    if (!isAssetDownloadEligible(asset, filetypes, tags)) continue
+    if (!isAssetDownloadEligible(asset, filetypes, tags)) continue;
 
-    cacheKey = `${config.typePrefix}-Asset-${asset._id}`
-    cacheData = await cache.get(cacheKey)
-    modified = asset.modified
+    cacheKey = `${config.typePrefix}-Asset-${asset._id}`;
+    cacheData = await cache.get(cacheKey);
+    modified = asset.modified;
 
     if (cacheData && modified === cacheData.modified) {
-      nodeId = cacheData.nodeId
-      touchNode({ nodeId })
+      nodeId = cacheData.nodeId;
+      touchNode({ nodeId });
     }
 
     if (!nodeId) {
-      url = cockpit.getApiUrl(`${trim(uploadPath, '/')}/${trim(asset.path, '/')}`)
+      url = cockpit.getApiUrl(
+        `${trim(uploadPath, "/")}/${trim(asset.path, "/")}`
+      );
 
       file = await createRemoteFileNode({
         url,
@@ -273,37 +301,51 @@ const fetchAssets = async (config, assetMap, cockpit, args) => {
         cache,
         createNode,
         createNodeId,
-      })
-      nodeId = file.id
+      });
+      nodeId = file.id;
 
       await cache.set(cacheKey, {
         nodeId,
         modified,
-      })
+      });
     }
-    assetMap[asset.path] = nodeId
+    assetMap[asset.path] = nodeId;
   }
 
-  return assetMap
-}
+  return assetMap;
+};
 
+const processEntry = (
+  TypeNode,
+  parent,
+  l10n,
+  specifications,
+  entry,
+  assetMap,
+  helpers
+) => {
+  const { processFieldValue } = helpers;
+  const { language, isDefault } = l10n;
 
-const processEntry = (TypeNode, parent, l10n, specifications, entry, assetMap, helpers) => {
-  const { processFieldValue } = helpers
-  const { language, isDefault } = l10n
+  const suffix = language ? `_${language}` : "";
+  let links = [],
+    nodes = [],
+    id = entry._id;
+  const node = TypeNode(
+    {
+      ...entry,
+      children: [],
+      id: `${id}${suffix}`,
+      language,
+    },
+    parent && { parent: parent.id }
+  );
 
-  const suffix = language ? `_${language}` : ''
-  let links = [], nodes = [], id = entry._id
-  const node = TypeNode({
-    ...entry,
-    children: [],
-    id: `${id}${suffix}`,
-    language,
-  }, parent && { parent: parent.id })
+  // console.log('specxx', specifications)
 
   // process field values, make nodes for repeaters, generate ids for relations
   for (let [field, spec] of Object.entries(specifications)) {
-    key = (spec.localize && !isDefault) ? `${field}${suffix}` : field
+    key = spec.localize && !isDefault ? `${field}${suffix}` : field;
 
     const result = processFieldValue(
       suffix,
@@ -312,39 +354,41 @@ const processEntry = (TypeNode, parent, l10n, specifications, entry, assetMap, h
       spec,
       field,
       node[key],
-      assetMap,
-    )
+      assetMap
+    );
 
-    delete(node[field])
-    node[result.field] = result.value
-    node.children = [].concat(node.children, result.nodes.map(n => n.id))
+    delete node[field];
+    node[result.field] = result.value;
+    node.children = [].concat(
+      node.children,
+      result.nodes.map((n) => n.id)
+    );
 
-    links = [].concat(links, result.links)
-    nodes = [].concat(nodes, result.nodes)
+    links = [].concat(links, result.links);
+    nodes = [].concat(nodes, result.nodes);
   }
 
-  return { node, nodes, links }
-}
-
+  return { node, nodes, links };
+};
 
 const isAssetDownloadEligible = (asset, filetypes, tags) => {
-  const suitableFiletype = Object.entries(filetypes).map(
-    ([key, value]) => (value === true && asset[key] === true)
-  ).includes(true)
-  if (!suitableFiletype) return false
+  const suitableFiletype = Object.entries(filetypes)
+    .map(([key, value]) => value === true && asset[key] === true)
+    .includes(true);
+  if (!suitableFiletype) return false;
 
-  const { whitelist, blacklist } = tags
+  const { whitelist, blacklist } = tags;
   if (whitelist.length) {
-    if (asset.tags.length === 0) return false
+    if (asset.tags.length === 0) return false;
     for (let i = 0; i < asset.tags.length; i++) {
-      if (whitelist.includes(asset.tags[i])) return true
+      if (whitelist.includes(asset.tags[i])) return true;
     }
-    return false
+    return false;
   }
   if (blacklist.length) {
     for (let i = 0; i < asset.tags.length; i++) {
-      if (blacklist.includes(asset.tags[i])) return false
+      if (blacklist.includes(asset.tags[i])) return false;
     }
   }
-  return true
-}
+  return true;
+};
--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
-
 const createNodeHelpers = require('gatsby-node-helpers').default
-
 
 const getContentHelpers = (args, config) => {
   const { typePrefix, uploadPath } = config
-  const { createNodeFactory, generateNodeId } = createNodeHelpers({ typePrefix })
+  const { createNodeFactory, generateNodeId } = createNodeHelpers({
+    typePrefix,
+  })
 
   const AssetNode = createNodeFactory('content Asset')
   const PlainNode = createNodeFactory('content Plain')
@@ -17,24 +17,24 @@ const getContentHelpers = (args, config) => {
     },
   }))
 
-  const getContentTypeNode = (type, middleware) => (
+  const getContentTypeNode = (type, middleware) =>
     createNodeFactory(type, middleware)
-  )
 
-  const getValueNode = (parent, type, label, value, assetMap={}) => {
+  const getValueNode = (parent, type, label, value, assetMap = {}) => {
     let node
     switch (type) {
       case 'asset':
         let id
-        Object.keys(assetMap).map((key) => {
-          if (key.includes(value.path.replace(uploadPath, ''))) id = assetMap[key]
+        Object.keys(assetMap).map(key => {
+          if (key.includes(value.path.replace(uploadPath, '')))
+            id = assetMap[key]
         })
         if (!id) break
 
         const { name, title, description: content } = value
         node = AssetNode(
           { label, id: value.id, asset___NODE: id, name, title, content },
-          { parent },
+          { parent }
         )
         break
 
@@ -51,13 +51,13 @@ const getContentHelpers = (args, config) => {
         break
 
       default:
-        console.warn('Could not find Node for type', type)
+        console.warn('Could not find Node for type', type, label)
         return
     }
     return node
   }
 
-  const getFieldSpecification = (field) => {
+  const getFieldSpecification = field => {
     const options = field.options || {}
     const specification = {
       type: field.type,
@@ -65,7 +65,7 @@ const getContentHelpers = (args, config) => {
       label: field.label || field.name,
       default: field.default || '',
       localize: field.localize || false,
-
+      options: field.options || false,
       link: false,
       multiple: false,
       asset: false,
@@ -73,30 +73,30 @@ const getContentHelpers = (args, config) => {
     }
 
     switch (field.type.toLowerCase()) {
-      case "text":
-      case "textarea":
+      case 'text':
+      case 'textarea':
         return specification
 
-      case "markdown":
+      case 'markdown':
         specification.remark = true
         return specification
 
-      case "collectionlink":
+      case 'collectionlink':
         specification.link = true
         specification.multiple = options.multiple || false
         specification.default = null
         if (options.multiple) specification.default = []
         return specification
 
-      case "repeater":
+      case 'repeater':
         specification.link = true
         specification.multiple = options.multiple || false
         specification.default = null
         if (options.multiple) specification.default = []
         return specification
 
-      case "asset":
-      case "image":
+      case 'asset':
+      case 'image':
         specification.default = null
         specification.asset = true
         return specification
@@ -106,46 +106,86 @@ const getContentHelpers = (args, config) => {
     }
   }
 
-  const processFieldValue = (suffix, parent, source, spec, sourceField, value, assetMap={}) => {
-    let id, processedValue, node, links = [], nodes = []
+  const processFieldValue = (
+    suffix,
+    parent,
+    source,
+    spec,
+    sourceField,
+    value,
+    assetMap = {}
+  ) => {
+    let id,
+      processedValue,
+      node,
+      links = [],
+      nodes = []
     let field = sourceField
 
-    const processValue = (suffix, parent, id, index, item, assetMap={}) => {
-      const { type, label, name } = item.field
+    // console.log('+++', value)
+
+    const processValue = (
+      suffix,
+      parent,
+      id,
+      index,
+      item,
+      assetMap = {},
+      spec
+    ) => {
+      // console.log('------', item)
+      const { field: { type, label } = {}, name } = spec
 
       if (type === 'asset') {
-        return getValueNode(parent, type, label, {
-          id: `${id}_${index}_${label}${suffix}`,
-          name,
-          ...item.value,
-        }, assetMap)
+        return getValueNode(
+          parent,
+          type,
+          label,
+          {
+            id: `${id}_${index}_${label}${suffix}`,
+            name,
+            ...item.value,
+          },
+          assetMap
+        )
       } else if (type === 'collectionlink') {
-        const { display: content, multiple } = item.field.options
+        const { display: content, multiple } = spec.options
 
         let values = item.value
         if (!multiple) values = [values]
-        values = values.map(value => generateNodeId(
-          `collection ${value.link}`,
-          `${value._id}${suffix}`
-        ))
+        values = values.map(value =>
+          generateNodeId(`collection ${value.link}`, `${value._id}${suffix}`)
+        )
 
-        return getValueNode(parent, type, label, {
-          id: `${id}_${index}_${label}${suffix}`,
-          name,
-          content,
-          links___NODE: values,
-        }, assetMap)
+        return getValueNode(
+          parent,
+          type,
+          label,
+          {
+            id: `${id}_${index}_${label}${suffix}`,
+            name,
+            content,
+            links___NODE: values,
+          },
+          assetMap
+        )
       }
 
-      return getValueNode(parent, type, label, {
-        id: `${id}_${index}_${label}${suffix}`,
-        name,
-        content: item.value,
-      }, assetMap)
+      return getValueNode(
+        parent,
+        type,
+        label,
+        {
+          id: `${id}_${index}_${label}${suffix}`,
+          name,
+          content: item.value,
+        },
+        assetMap
+      )
     }
 
     switch (spec.type) {
-      case "collectionlink":
+      case 'collectionlink':
         field = `${field}___NODE`
         if (!value) break
 
@@ -158,54 +198,62 @@ const getContentHelpers = (args, config) => {
           break
         }
 
-        processedValue = value.map(v => generateNodeId(
-          `collection ${v.link}`,
-          `${v._id}${suffix}`
-        ))
+        processedValue = value.map(v =>
+          generateNodeId(`collection ${v.link}`, `${v._id}${suffix}`)
+        )
         links = [].concat(links, processedValue)
         break
 
-      case "asset":
-      case "image":
+      case 'asset':
+      case 'image':
         field = `${field}___NODE`
         if (!value) break
 
-        Object.keys(assetMap).map((key) => {
-          if (key.includes(value.path.replace(uploadPath, ''))) id = assetMap[key]
+        Object.keys(assetMap).map(key => {
+          if (key.includes(value.path.replace(uploadPath, '')))
+            id = assetMap[key]
         })
         processedValue = id
         break
 
-      case "text":
-      case "textarea":
+      case 'text':
+      case 'textarea':
         processedValue = (value && String(value)) || ''
         break
 
-      case "markdown":
+      case 'markdown':
         field = `${field}___NODE`
 
         id = `${source}_${spec.label}${suffix}`
-        node = RemarkNode({
-          id,
-          content: (value && String(value)) || '',
-        }, { parent })
+        node = RemarkNode(
+          {
+            id,
+            content: (value && String(value)) || '',
+          },
+          { parent }
+        )
         processedValue = node.id
         nodes.push(node)
         break
 
-      case "repeater":
+      case 'repeater':
         field = `${field}___NODE`
 
         if (!value) break
 
-        nodes = value.map((v, i) => processValue(
-          suffix,
-          parent,
-          `${source}_${spec.label}`,
-          i,
-          v,
-          assetMap,
-        )).filter(n => n)
+        nodes = value
+          .map((v, i) =>
+            processValue(
+              suffix,
+              parent,
+              `${source}_${spec.label}`,
+              i,
+              v,
+              assetMap,
+              spec
+            )
+          )
+          .filter(n => n)
         processedValue = nodes.map(n => n.id)
         break
 
@@ -229,6 +277,5 @@ const getContentHelpers = (args, config) => {
     },
   }
 }
-
 
 module.exports = getContentHelpers
